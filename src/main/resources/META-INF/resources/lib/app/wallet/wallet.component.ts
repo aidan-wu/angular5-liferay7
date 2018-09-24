d52aa855f0f3fbc0ead9516184fcb81ce8e90258
Daniel Wu <daniel.wu@coxautoinc.com>
2018-09-23 23:09:42 -0400

    added

---
+++ src/main/resources/META-INF/resources/lib/app/wallet/wallet.component.ts
@@ -0,0 +1,26 @@
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+@Component({
+    selector: 'wallet',
+    templateUrl: '/o/angular-npm-portlet/lib/app/wallet/wallet.component.html',
+    styleUrls: ['/o/angular-npm-portlet/lib/app/wallet/wallet.component.css']
+})
+
+export class WalletComponent {
+    model: any = {};
+    loading = false;
+
+    constructor(
+        private router: Router){}
+
+    submit(theForm: NgForm) {
+        alert(`Hello ${theForm.value.name}!`);
+    }
+
+
+    register() {
+       return null;
+    }
+}
